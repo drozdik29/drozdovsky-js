61ee0a83ce9424fd0f67bfad49738bef4bb5037a
Max Drozdovsky <drozdovskiimax@gmail.com>
2023-08-25 14:05:21 +0300

    1

--- test/vhid_test.js
+++ test/vhid_test.js
@@ -4,6 +4,6 @@ Scenario('test something',  ({ I }) => {
     I.amOnPage('https://fcdynamo.com/');
     pause();
     I.click({xpath:'//*[@id="slick-slide10"]/h3/a'});
-    let text = I.grabTextFrom ({xpath:'/html/body/main/section/div/div/div[1]/div[1]/div[1]/div[2]/h1'})
-    console.log(text)
+    I.click({xpath:'/html/body/header/div/div/div/div[2]/div[2]/div[2]/ul/li[1]/div/button/div/div/div'});
+    I.fillField({xpath:"/html/body/header/div/div/div/div[2]/div[2]/div[2]/ul/li[1]/div/div/div/form/input[2]"},'Max');
 });
