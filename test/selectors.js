9bd57c0c18cddb31a0df50780e419b7ae40875ed
Max Drozdovsky <drozdovskiimax@gmail.com>
2023-08-27 13:48:30 +0300

    done homework

---
+++ test/selectors.js
@@ -0,0 +1,15 @@
+module.exports = {
+    myAccount:'//*[@id="stuck"]/div/div/div/div/div/div/div/div/ul/li[10]/a',
+    registration:'//*[@id="content"]/div/div[1]/div/a',
+    firstName: '//*[@id="input-firstname"]',
+    lastName:'//*[@id="input-lastname"]',
+    email:'//*[@id="input-email"]',
+    phoneNumber:'//*[@id="input-telephone"]',
+    inputPassword:'//*[@id="input-password"]',
+    confirmPassword:'//*[@id="input-confirm"]',
+    yesButton:'//*[@id="content"]/form/fieldset[3]/div/div/label[1]',
+    noButton:'//*[@id="content"]/form/fieldset[3]/div/div/label[2]',
+    privacyPolicyAgreement:'//*[@id="content"]/form/div/div/input[1]',
+    continueButton:'//*[@id="content"]/form/div/div/input[2]'
+  };
+ 
--- test/vhid_test.js
+++ test/vhid_test.js
@@ -1,5 +1,5 @@
 Feature('registration');
-
+  const selectors = require('./selectors.js');
 Scenario(' registration ',  ({ I }) => {
     const today = new Date();
     const year = today.getFullYear();
@@ -8,23 +8,23 @@ Scenario(' registration ',  ({ I }) => {
     const hours = String(today.getHours()).padStart(2, '0');
     const minutes = String(today.getMinutes()).padStart(2, '0');
     const seconds = String(today.getSeconds()).padStart(2, '0');
-  
-    const formattedDateTime = `${year}-${month}-${day}-${hours}-${minutes}-${seconds}`;
+
+    const formattedDateTime = `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
     const email = `${formattedDateTime}@gmail.com`;
     I.amOnPage('http://opencart.qatestlab.net/');
     I.resizeWindow(1680, 1050);
-    I.click({xpath:'//*[@id="stuck"]/div/div/div/div/div/div/div/div/ul/li[10]/a'});
-    I.click({xpath:'//*[@id="content"]/div/div[1]/div/a'});
-    I.fillField({xpath:'//*[@id="input-firstname"]'},'Max');
-    I.fillField({xpath:'//*[@id="input-lastname"]'},'123');
-    I.fillField({xpath:'//*[@id="input-email"]'},email);
-    I.fillField({xpath:'//*[@id="input-telephone"]'},'+380667869040');
-    I.fillField({xpath:'//*[@id="input-password"]'},'53x4am5F8dhdF');
-    I.fillField({xpath:'//*[@id="input-confirm"]'},'53x4am5F8dhdF');
-    I.click({xpath:'//*[@id="content"]/form/fieldset[3]/div/div/label[1]'});
-    I.click({xpath:'//*[@id="content"]/form/fieldset[3]/div/div/label[2]'});
-    I.click({xpath:'//*[@id="content"]/form/div/div/input[1]'});
-    I.click({xpath:'//*[@id="content"]/form/div/div/input[2]'});
+    I.click(selectors.myAccount);
+    I.click(selectors.registration);
+    I.fillField(selectors.firstName,'Max');
+    I.fillField(selectors.lastName,'123');
+    I.fillField(selectors.email,email);
+    I.fillField(selectors.phoneNumber,'+380667869040');
+    I.fillField(selectors.inputPassword,'53x4am5F8dhdF');
+    I.fillField(selectors.confirmPassword,'53x4am5F8dhdF');
+    I.click(selectors.yesButton);
+    I.click(selectors.noButton);
+    I.click(selectors.privacyPolicyAgreement);
+    I.click(selectors.continueButton);
     pause();
 });
 
